8a85570782dae2944edddffaa5ec215789112d3c
Ben Rubin <benrubin@Bens-MacBook-Pro-3.local>
2016-11-23 03:51:15 -0600

    check if paths is an array before adding ‘?include’

--- src/services/util.js
+++ src/services/util.js
@@ -68,6 +68,7 @@ function jamUtil(jamStorage, jamKeys) {
 
   // builds url with ids and includes
   function createGetUrl(options, id) {
+    var paths;
     var getUrl = options.url;
     id = options.id || id;
 
@@ -75,11 +76,14 @@ function jamUtil(jamStorage, jamKeys) {
     if (options.include instanceof Array && options.include.length > 0) {
       getUrl += '?include=' + options.include.join(',');
     } else if (typeof options.schema === 'object' && options.schema !== null) {
-      getUrl += '?include=' + getAllObjectPaths(options.schema, '').filter(function (arr) {
-        return arr.length;
-      }).map(function (arr) {
-        return arr.join('.');
-      }).join(',');
+      paths = getAllObjectPaths(options.schema, '');
+      if (paths instanceof Array) {
+        getUrl += '?include=' + paths.filter(function (arr) {
+          return arr.length;
+        }).map(function (arr) {
+          return arr.join('.');
+        }).join(',');
+      }
     }
 
     return getUrl;
